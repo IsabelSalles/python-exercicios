246a643e0df346af22ef06a0e5eac45dd5acc307
Isabel Salles <isabelsallessantos@gmail.com>
2026-04-12 00:17:49 -0300

    implementado calculadora simples

---
+++ logica-basica/calculadora_simples.py
@@ -0,0 +1,13 @@
+name = input("Insira seu nome: " )
+print(f"Olá {name}")
+number1 = int(input("Insira um número: "))
+number2 = int(input("Insira outro número: "))
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print(f"A soma dos números é: {sum}")
+print(f"A subtração dos numeros é: {subtraction}")
+print(f"A multiplicação dos números é: {multiplication}")
+print(f"A divisão dos números é: {division}")
